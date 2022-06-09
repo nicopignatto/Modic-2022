ba5dccd95ab08a12870e58b22b4a78f9ca5f89d3
ElSala <joaquin2001.js@gmail.com>
2022-06-09 11:37:51 -0300

    first person y movimiento

---
+++ Assets/Scripts/Camaralook.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camaralook : MonoBehaviour
+{
+    public float mousesensivility = 1500f;
+    public Transform playerbody;
+    float xRotation = 0f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float mousex = Input.GetAxis("Mouse X") * mousesensivility * Time.deltaTime;
+        float mousey = Input.GetAxis("Mouse Y") * mousesensivility * Time.deltaTime;
+        xRotation -= mousey;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerbody.Rotate(Vector3.up * mousex);
+    }
+}
---
+++ Assets/Scripts/Playermove.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+    public float gravity = -9.8f;
+    public float jumpheight = 2f;
+    public Transform groundcheck;
+    public float grounddistance = 0.3f;
+    public LayerMask groundmask;
+    Vector3 velocity;
+    bool isgrounded;
+
+    void Start()
+    {
+      
+    }
+
+  
+    void Update()
+    {
+        isgrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundmask);
+        
+        if(isgrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump")&& isgrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpheight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
