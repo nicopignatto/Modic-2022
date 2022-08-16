94ee4fc448fc91adf7d0d6f9bbf6eae733d43e27
nicopignatto <81388990+nicopignatto@users.noreply.github.com>
2022-08-15 23:34:15 -0300

    Implementacion de una trampa movil
    Falta hacer que cuando el jugador colisiona con la trampa se muera o se destruya el objeto PJ.

---
+++ Assets/Scripts/Trampas/TrampaMovil.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampaMovil : MonoBehaviour
+{
+    [Header("Vel de movimiento de la trampa")]
+    [SerializeField] private float velMovTrampaMovil;
+
+    [Header("Limites de movimiento de la trampa movil")]
+    [SerializeField] private float limiteIzq;
+    [SerializeField] private float limiteDer;
+
+    [Header("Linkeos")]
+    [SerializeField] private Rigidbody rbTrampaMovil;
+    //[SerializeField] private SensorTrampa sensorTrampaMovil;
+
+    //variables privadas
+    private bool irAIzquierda;
+    void FixedUpdate()
+    {
+        MovimientoTrampaMovil();   
+    }
+
+    private void MovimientoTrampaMovil()
+    {
+        if (SensorTrampa.ColisioneConAlgo == true)
+        {
+            if (transform.position.z > limiteDer)
+            {
+                irAIzquierda = true;
+            }
+            else
+            {
+                if (transform.position.z < limiteIzq)
+                {
+                    irAIzquierda = false;
+                }
+            }
+
+            if (irAIzquierda)
+            {
+                transform.Translate(Vector3.back * velMovTrampaMovil);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * velMovTrampaMovil);
+            }
+
+        }
+    }
+}
