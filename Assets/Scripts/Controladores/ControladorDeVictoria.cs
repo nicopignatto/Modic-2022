d2f688ca5996d98a47b430edd53daf756d05cfb0
ElSala <joaquin2001.js@gmail.com>
2022-10-03 11:40:00 -0300

    se agrego el panel de victoria y se ajust unos valores random

--- Assets/Scripts/Controladores/ControladorDeVictoria.cs
+++ Assets/Scripts/Controladores/ControladorDeVictoria.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.UI;
+using System;
 public class ControladorDeVictoria : MonoBehaviour
 {
     [SerializeField] GameObject panel;
     public bool isactivated = false;
 
-    private void Start()
-    {
-        panel = GameObject.Find("PantallaDeVictoria");
-    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("funciona");
-        if (other.tag == "Player" && !isactivated)
+        if (other.tag == "Jugador" && !isactivated)
         {
             isactivated = true;
 
